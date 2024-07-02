973f48639dc8082fda1099b695af80af9f5b9d2f
Shilpa Munjal <shilpamunjal241@gmail.com>
2024-07-03 01:57:44 +0530

    Created Comment UI Page and service to call backend api.

--- src/app/pages/view-post/view-post.component.ts
+++ src/app/pages/view-post/view-post.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { PostService } from '../../service/post.service';
 import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommentService } from '../../service/comment.service';
 
 @Component({
   selector: 'app-view-post',
@@ -10,11 +12,24 @@ import { ActivatedRoute } from '@angular/router';
 export class ViewPostComponent {
   postId=this.activatedroute.snapshot.params['id'];
   postData:any;
+  commentForm!:FormGroup;
 
-  constructor(private postservice:PostService,private activatedroute:ActivatedRoute){}
+  constructor(private postservice:PostService,private activatedroute:ActivatedRoute,private fb:FormBuilder,private commentservice:CommentService){}
   ngOnInit(){
     console.log(this.postId);
     this.getPostById();
+    this.commentForm=this.fb.group({
+      postedBy:[null,Validators.required],
+      content:[null,Validators.required]
+    })
+  }
+
+  publishComment(){
+    console.log("publish comment");
+    const postedBy=this.commentForm.get('postedBy')?.value;
+    const content=this.commentForm.get('content')?.value;
+    console.log(content);
+    this.commentservice.createComment(this.postId,postedBy,content).subscribe(res=>console.log(res));
   }
   getPostById(){
   this.postservice.getPostById(this.postId).subscribe(res =>{
---
+++ src/app/service/comment.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+const BASIC_URL="http://localhost:8088/";
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+
+  constructor(private http:HttpClient) { }
+
+  createComment(postid:number,postedBy:string,content:string):Observable<any>{
+    const params={
+      postid:postid,
+      postedBy:postedBy
+    }
+    return this.http.post(BASIC_URL+`api/comments/create`,content,{params});
+  }
+
+  getAllCommentsByPostId(postid:number):Observable<any>{
+    return this.http.get(BASIC_URL+`api/comments/{postid}`);
+
+  }
+}
